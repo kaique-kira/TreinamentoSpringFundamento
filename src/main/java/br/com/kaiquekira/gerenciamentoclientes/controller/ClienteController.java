9055ee29774ed9f4c9a8c977d46a4fbe23683959
Kaique Kira <83593559+kaique-kira@users.noreply.github.com>
2022-10-04 12:28:31 -0300

    feat.adicioando interfaces

--- src/main/java/br/com/kaiquekira/gerenciamentoclientes/controller/ClienteController.java
+++ src/main/java/br/com/kaiquekira/gerenciamentoclientes/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +24,24 @@ public class ClienteController {
         ModelAndView modelAndView = new ModelAndView("cliente/listar.html");
         List<Cliente> clientes = clienteRepository.findAll();
         modelAndView.addObject("clientes", clientes);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("cliente/detalhar.html");
+
+        Cliente cliente = clienteRepository.getReferenceById(id);
+        modelAndView.addObject("cliente", cliente);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}/excluir")
+    public ModelAndView excluir(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("redirect: /cliente");
+        clienteRepository.deleteById(id);
 
         return modelAndView;
     }
